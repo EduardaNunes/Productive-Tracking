03f233fd463037d8a3949223aaf5e759835b4c10
Duda Nunes <eduardapmn@gmail.com>
2023-05-18 02:45:20 -0300

    Responsivo

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ const topCardImg = document.querySelector('.top-card-img')
 const submitBtn = document.querySelector('#submitBtn')
 const btnSubmitText = document.querySelectorAll('.btn-submit-text')
 
-function ChangetoRegister(){
+const btnChangetoRegister = document.querySelector('#change-to-register')
+const btnChangetoLogin = document.querySelector('#change-to-login')
+
+btnChangetoRegister.addEventListener('click',()=>{
+
     topCard.classList.add('top-card-position-left')
     topCardImg.classList.add('top-card-img-position-left')
     submitBtn.classList.add('submit-button-rotation')
@@ -12,9 +16,11 @@ function ChangetoRegister(){
         btnSubmitText[0].classList.add('hide')
         btnSubmitText[1].classList.remove('hide')
     },250)
-}
 
-function ChangetoLogin(){
+})
+
+btnChangetoLogin.addEventListener('click',()=>{
+
     topCard.classList.remove('top-card-position-left')
     topCardImg.classList.remove('top-card-img-position-left')
     submitBtn.classList.remove('submit-button-rotation')
@@ -23,4 +29,5 @@ function ChangetoLogin(){
         btnSubmitText[0].classList.remove('hide')
         btnSubmitText[1].classList.add('hide')
     },250)
-}
+
+})
