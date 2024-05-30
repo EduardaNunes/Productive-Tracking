7b64bb1385569165d0ceb5407b5d5c80ac7a886d
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2024-05-30 15:56:06 -0300

    Changed location.replace to location.assign

--- Js/login.js
+++ Js/login.js
@@ -5,7 +5,7 @@ onAuthStateChanged(auth, (user) => {
 
     if (user) {
         console.log('User Is Signed In')
-        location.replace("../Pages/main_page.html")
+        location.assign("/Pages/main_page.html")
     }          
 })
 
@@ -56,7 +56,7 @@ function login(username, password){
       console.log(user.uid)
 
       window.alert('Conta Logada com Sucesso')
-      location.replace("../Pages/main_page.html") 
+      location.assign("/Pages/main_page.html") 
 
     })
     .catch((error) => {
